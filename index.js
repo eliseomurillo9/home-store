754146a170f1e096257a8b2e6289ab164c93654c
Eliseo Murillo <eliseo.murillo@adeo.com>
2023-04-20 07:29:26 +0200

    feat(cors): add cors to api

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 import express, { json } from "express";
-import productsRouter from './api/routes/productsRoutes.js';
-import dbConnection from './api/db/index.js';
-import categoryRouter from './api/routes/categoryRoutes.js';
-import subCategoryRouter from './api/routes/subCategoryRoutes.js';
-import carrouselRouter from './api/backoffice/routes/carrouselRoutes.js'
-import { handler as ssrHandler } from './dist/server/entry.mjs';
-import dotenv from 'dotenv';
+import productsRouter from "./api/routes/productsRoutes.js";
+import dbConnection from "./api/db/index.js";
+import categoryRouter from "./api/routes/categoryRoutes.js";
+import subCategoryRouter from "./api/routes/subCategoryRoutes.js";
+import carrouselRouter from "./api/backoffice/routes/carrouselRoutes.js";
+import { handler as ssrHandler } from "./dist/server/entry.mjs";
+import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 dbConnection();
+
+app.use(json());
+app.use(cors({
+  origin:'http://localhost:5000'
+}));
 app.get("/api/test", (req, res) => {
-      res.send("Home store HELLOOOO");
-    });
-    
-app.use(json()); // for parsing application/json
-app.use('/', productsRouter);
-app.use('/categories', categoryRouter);
-app.use('/subCategories', subCategoryRouter);
-app.use('/carrousel', carrouselRouter)
+  res.send("Home store HELLOOOO");
+});
+app.use("/", productsRouter);
+app.use("/categories", categoryRouter);
+app.use("/subCategories", subCategoryRouter);
+app.use("/carrousel", carrouselRouter);
 
-
-
-app.use(express.static('dist/client/'));
-app.use('/uploads', express.static('uploads'));
+app.use(express.static("dist/client/"));
+app.use("/uploads", express.static("uploads"));
 app.use(ssrHandler);
 
-
-app.listen(PORT, () => console.log(`Server started on port ${process.env.PORT}`));
-
-
+app.listen(PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
